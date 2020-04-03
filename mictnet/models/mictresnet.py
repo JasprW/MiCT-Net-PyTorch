ad172f27178142787ae5ad33681d189ec95158c0
Jaspr Wang <xiaop987@gmail.com>
2020-04-03 22:59:18 +0800

    Fix residual channels

--- mictnet/models/mictresnet.py
+++ mictnet/models/mictresnet.py
@@ -375,6 +375,7 @@ class MiCTBlock(nn.Module):
                                        padding=0, bias=False)
         self.bn = nn.BatchNorm3d(planes)
         self.relu = nn.ReLU(inplace=True)
+        self.conv_r = nn.Conv3d(inplanes, planes, kernel_size=1, stride=1, padding=1, bias=False)
 
     def forward(self, x):
         residual = x
@@ -402,6 +403,7 @@ class MiCTBlock(nn.Module):
             out = self.bottlenecks[i](out)
         out = _to_5d_tensor(out, depth)
 
+        residual = self.conv_r(residual)
         out += residual
         out = self.relu(out)
         return out
